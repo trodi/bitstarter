c5f3e28817e5900f7c7a6d0a2307b58350c4a596
trodi <troy.t.mckinnon@gmail.com>
2013-07-12 20:32:33 -0500

    added support for cmd line parameter url to grade

--- grader.js
+++ grader.js
@@ -19,11 +19,15 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+   
+ + restler
+   - https://github.com/danwrong/restler
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,17 +40,29 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
+var loadCheerioFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+var loadCheerioUrl = function(htmlUrl, checksFile) {
+	var out = "";
+	restler.get(htmlUrl).on('complete', function(data) {
+		out = data;
+		//TODO: have this path use the same code as the other
+		var checkJson = checkHtml(cheerio.load(out), checksFile);
+		var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	});
+	return cheerio.load(out);
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
+var checkHtml = function(html, checksFile) {
+	$ = html;
+    var checks = loadChecks(checksFile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
@@ -65,10 +81,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to grade.')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	//TODO: if they specify both -f and -u
+    var checkJson;
+    console.log("checks file: %s", program.checks);
+    if (program.url) {
+    	loadCheerioUrl(program.url, program.checks);
+//    	checkJson = checkHtml(loadCheerioUrl(program.url), program.checks);
+    } else if (program.file) {
+    	checkJson = checkHtml(loadCheerioFile(program.file), program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
